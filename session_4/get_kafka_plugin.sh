12332d883df8af084c295120d58c3cf1af5cb014
Nghia Doan <hong.nghiadoan@da-an.ca>
2019-07-03 15:44:42 -0400

    Initial version.

---
+++ session_4/get_kafka_plugin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl --fail --silent --show-error --location --remote-name "https://github.com/neo4j-contrib/neo4j-streams/releases/download/3.5.3/neo4j-kafka-connect-neo4j-1.0.3.zip"
+mv neo4j-kafka-connect-neo4j-1.0.3.zip plugins/.
+cd plugins
+unzip neo4j-kafka-connect-neo4j-1.0.3.zip
+cd ..
