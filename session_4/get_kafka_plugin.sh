99a51d209450abea5e14f15331de488615c6e709
Nghia Doan <hong.nghiadoan@da-an.ca>
2019-07-03 20:26:18 -0400

    Make sure Kafka plugin is added first.

--- session_4/get_kafka_plugin.sh
+++ session_4/get_kafka_plugin.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+mkdir plugins
 curl --fail --silent --show-error --location --remote-name "https://github.com/neo4j-contrib/neo4j-streams/releases/download/3.5.3/neo4j-kafka-connect-neo4j-1.0.3.zip"
 mv neo4j-kafka-connect-neo4j-1.0.3.zip plugins/.
 cd plugins
 unzip neo4j-kafka-connect-neo4j-1.0.3.zip
 cd ..
+chmod 777 plugins
+rm plugins/neo4j-kafka-connect-neo4j-1.0.3.zip
