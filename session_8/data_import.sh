61e060463562b637b67a9914dc7a5b605bb4c3fd
NGHIA DOAN <hong.nghiadoan@da-an.ca>
2019-07-28 16:12:01 -0400

    Added tasks.

--- session_8/data_import.sh
+++ session_8/data_import.sh
@@ -4,11 +4,30 @@ NEO4J_DIR=~/neo4j
 
 if [ $# -lt 1 ]; then
   if [[ ! -d "$NEO4J_DIR" ]]; then
-    echo "Usage: ./data_import.sh <neo4j directory>"
+    echo "Usage: ./data_import.sh <neo4j directory> <COMMANDS>"
+    echo "  COMMAND: "
+    echo "      n: normalize data"
+    echo "      e: extract entities and relationships"
+    echo "      i: import data"
+    echo "      a: after import processing"
+    echo "      s: export statistics"
+    echo "  EXAMPLES:"
+    echo "      ./data_import.sh n: normalize registration & survey data only"
+    echo "      ./data_import.sh e: extract entities and relationships only"
+    echo "      ./data_import.sh i: import data only"
+    echo "      ./data_import.sh a: after import processing only"
+    echo "      ./data_import.sh s: export statistics only"
+    echo "      ./data_import.sh rneis: run a n -> e -> i -> a -> s pipeline"
     exit
   fi
 else
   NEO4J_DIR=$1
+fi
+
+if [ $# -lt 2 ]; then
+  commands=neias
+else
+  commands=$2
 fi
 
 res1=$(date +%s)
@@ -18,18 +37,24 @@ if [[ ! -d "$NEO4J_DIR/data" ]]; then
   chmod -R 777 $NEO4J_DIR/data
 fi
 
-if [ ! -f "$NEO4J_DIR/import/csps/registration_data.tsv" ] || [ ! -f "$NEO4J_DIR/import/csps/survey_data.tsv" ] ; then
-  echo "Required data files in $NEO4J_DIR/import/ directory: registration_data.tsv, survey_data.tsv"
-  exit
+if [[ $commands == *"r"* ]]; then
+  if [ ! -f "$NEO4J_DIR/import/csps/registration_data.tsv" ] || [ ! -f "$NEO4J_DIR/import/csps/survey_data.tsv" ] ; then
+    echo "Required data files in $NEO4J_DIR/import/ directory: registration_data.tsv, survey_data.tsv"
+    exit
+  fi
 fi
 
-printf "\nNormalize registration & survey data ...\n"
-  ./import_scripts/header_normalizer.sh $NEO4J_DIR/import/csps/registration_data.tsv $NEO4J_DIR/import/csps/survey_data.tsv
-printf "Done.\n"
+if [[ $commands == *"n"* ]]; then
+  printf "\nNormalize registration & survey data ...\n"
+    ./import_scripts/header_normalizer.sh $NEO4J_DIR/import/csps/registration_data.tsv $NEO4J_DIR/import/csps/survey_data.tsv
+  printf "Done.\n"
+fi
 
-printf "Extracting entities and relationships ...\n"
-  python import_scripts/prepare_data.py $NEO4J_DIR/import/csps/registration_data.tsv $NEO4J_DIR/import/csps/survey_data.tsv
-printf "Done.\n"
+if [[ $commands == *"e"* ]]; then
+  printf "Extracting entities and relationships ...\n"
+    python import_scripts/prepare_data.py $NEO4J_DIR/import/csps/registration_data.tsv $NEO4J_DIR/import/csps/survey_data.tsv
+  printf "Done.\n"
+fi
 
 HTTP_HOST_PORT=localhost:7474
 BOLT_HOST_PORT=localhost:7687
@@ -38,95 +63,101 @@ PASSWORD="##dis@da2019##"
 CONTAINER_NAME=jotunheimr
 CURRENT=$PWD
 
-docker-compose stop $CONTAINER_NAME \
-&& sudo rm -f $NEO4J_DIR/logs/import_report.log \
-&& cd $NEO4J_DIR/data \
-&& sudo rm -rf databases \
-&& cd $CURRENT \
-&& docker-compose up -d $CONTAINER_NAME
+if [[ $commands == *"i"* ]]; then
+  docker-compose stop $CONTAINER_NAME \
+  && sudo rm -f $NEO4J_DIR/logs/import_report.log \
+  && cd $NEO4J_DIR/data \
+  && sudo rm -rf databases \
+  && cd $CURRENT \
+  && docker-compose up -d $CONTAINER_NAME
 
-./import_scripts/wait_for_neo4j.sh $CONTAINER_NAME
+  ./import_scripts/wait_for_neo4j.sh $CONTAINER_NAME
 
-docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/neo4j-admin import \
-    --database=csps_survey \
-    --nodes:BusinessType /import/csps/business_type.tsv \
-    --nodes:City /import/csps/city.tsv \
-    --nodes:ClassificationGroup /import/csps/classification_group.tsv \
-    --nodes:Classification /import/csps/classification.tsv \
-    --nodes:Course /import/csps/course.tsv \
-    --nodes:DeliveryType /import/csps/delivery_type.tsv \
-    --nodes:Department /import/csps/department.tsv \
-    --nodes:Instructor /import/csps/instructor.tsv \
-    --nodes:Language /import/csps/language.tsv \
-    --nodes:Learner /import/csps/learner.tsv \
-    --nodes:Offering /import/csps/offering.tsv \
-    --nodes:Province /import/csps/province.tsv \
-    --nodes:Question /import/csps/question.tsv \
-    --nodes:Region /import/csps/region.tsv \
-    --nodes:Registration /import/csps/registration.tsv \
-    --nodes:Survey /import/csps/survey.tsv \
-    --relationships:BUSINESS_TYPE_OF /import/csps/business_type_TO_course.tsv \
-    --relationships:LOCATED_IN /import/csps/city_TO_learner.tsv \
-    --relationships:OFFERED_IN /import/csps/city_TO_offering.tsv \
-    --relationships:CLASSIFICATION_GROUP_OF /import/csps/classification_group_TO_classification.tsv \
-    --relationships:CLASSIFICATION_OF /import/csps/classification_TO_learner.tsv \
-    --relationships:COURSE_OF /import/csps/course_TO_offering.tsv \
-    --relationships:DELIVERY_TYPE_OF /import/csps/delivery_type_TO_course.tsv \
-    --relationships:DEPARTMENT_OF /import/csps/department_TO_learner.tsv \
-    --relationships:INSTRUCTOR_OF /import/csps/instructor_TO_offering.tsv \
-    --relationships:LANGUAGE_OF /import/csps/language_TO_offering.tsv \
-    --relationships:LEARNER_OF /import/csps/leaner_TO_registration.tsv \
-    --relationships:REGISTERED_FOR /import/csps/offering_TO_registration.tsv \
-    --relationships:SURVEYED_FOR /import/csps/offering_TO_survey.tsv \
-    --relationships:PROVINCE_OF /import/csps/province_TO_city.tsv \
-    --relationships:QUESTION_OF /import/csps/question_TO_survey.tsv \
-    --relationships:REGION_OF /import/csps/region_TO_province.tsv \
-    --delimiter TAB --array-delimiter "|" --multiline-fields true \
-    --ignore-missing-nodes=true \
-    --report-file=/logs/import_report.log \
-    --quote '"'
+  docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/neo4j-admin import \
+      --database=csps_survey \
+      --nodes:BusinessType /import/csps/business_type.tsv \
+      --nodes:City /import/csps/city.tsv \
+      --nodes:ClassificationGroup /import/csps/classification_group.tsv \
+      --nodes:Classification /import/csps/classification.tsv \
+      --nodes:Course /import/csps/course.tsv \
+      --nodes:DeliveryType /import/csps/delivery_type.tsv \
+      --nodes:Department /import/csps/department.tsv \
+      --nodes:Instructor /import/csps/instructor.tsv \
+      --nodes:Language /import/csps/language.tsv \
+      --nodes:Learner /import/csps/learner.tsv \
+      --nodes:Offering /import/csps/offering.tsv \
+      --nodes:Province /import/csps/province.tsv \
+      --nodes:Question /import/csps/question.tsv \
+      --nodes:Region /import/csps/region.tsv \
+      --nodes:Registration /import/csps/registration.tsv \
+      --nodes:Survey /import/csps/survey.tsv \
+      --relationships:BUSINESS_TYPE_OF /import/csps/business_type_TO_course.tsv \
+      --relationships:LOCATED_IN /import/csps/city_TO_learner.tsv \
+      --relationships:OFFERED_IN /import/csps/city_TO_offering.tsv \
+      --relationships:CLASSIFICATION_GROUP_OF /import/csps/classification_group_TO_classification.tsv \
+      --relationships:CLASSIFICATION_OF /import/csps/classification_TO_learner.tsv \
+      --relationships:COURSE_OF /import/csps/course_TO_offering.tsv \
+      --relationships:DELIVERY_TYPE_OF /import/csps/delivery_type_TO_course.tsv \
+      --relationships:DEPARTMENT_OF /import/csps/department_TO_learner.tsv \
+      --relationships:INSTRUCTOR_OF /import/csps/instructor_TO_offering.tsv \
+      --relationships:LANGUAGE_OF /import/csps/language_TO_offering.tsv \
+      --relationships:LEARNER_OF /import/csps/leaner_TO_registration.tsv \
+      --relationships:REGISTERED_FOR /import/csps/offering_TO_registration.tsv \
+      --relationships:SURVEYED_FOR /import/csps/offering_TO_survey.tsv \
+      --relationships:PROVINCE_OF /import/csps/province_TO_city.tsv \
+      --relationships:QUESTION_OF /import/csps/question_TO_survey.tsv \
+      --relationships:REGION_OF /import/csps/region_TO_province.tsv \
+      --delimiter TAB --array-delimiter "|" --multiline-fields true \
+      --ignore-missing-nodes=true \
+      --report-file=/logs/import_report.log \
+      --quote '"'
 
-if [[ -f "$NEO4J_DIR/logs/import_report.log" ]]; then
-  printf "Import issues:\n"
-  cat $NEO4J_DIR/logs/import_report.log
+  if [[ -f "$NEO4J_DIR/logs/import_report.log" ]]; then
+    printf "Import issues:\n"
+    cat $NEO4J_DIR/logs/import_report.log
+  fi
+
+  docker-compose stop $CONTAINER_NAME \
+  && cd $NEO4J_DIR/data \
+  && sudo rm -rf databases/graph.db \
+  && sudo mv databases/csps_survey databases/graph.db \
+  && sudo chmod -R 777 databases \
+  && cd $CURRENT \
+  && docker-compose up -d $CONTAINER_NAME
+
+  ./import_scripts/wait_for_neo4j.sh $CONTAINER_NAME
 fi
 
-docker-compose stop $CONTAINER_NAME \
-&& cd $NEO4J_DIR/data \
-&& sudo rm -rf databases/graph.db \
-&& sudo mv databases/csps_survey databases/graph.db \
-&& sudo chmod -R 777 databases \
-&& cd $CURRENT \
-&& docker-compose up -d $CONTAINER_NAME
+if [[ $commands == *"a"* ]]; then
+  printf "Creating initial schema ...\n"
+  (docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/data_schema.cql
+  printf "Done.\n"
 
-./import_scripts/wait_for_neo4j.sh $CONTAINER_NAME
+  printf "Convert data ...\n"
+  (docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/data_convert_simple.cql
+  printf "Done.\n"
 
-printf "Creating initial schema ...\n"
-(docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/data_schema.cql
-printf "Done.\n"
+  printf "Normalize data ...\n"
+  (docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/data_normalization.cql
+  printf "Done.\n"
 
-if [[ ! -d "$NEO4J_DIR/import/csps/reports/" ]]; then
-  printf "Create report directory ...\n"
-  sudo mkdir $NEO4J_DIR/import/csps/reports
-  printf "Grant access ...\n"
-  sudo chmod 777 $NEO4J_DIR/import/csps/reports
+  printf "Snapshot report ...\n"
+  (docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/graph_db_report.cql
+  printf "Done.\n"
 fi
 
-printf "Convert data ...\n"
-(docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/data_convert_simple.cql
-printf "Done.\n"
+if [[ $commands == *"s"* ]]; then
+  if [[ ! -d "$NEO4J_DIR/import/csps/reports/" ]]; then
+    printf "Create report directory ...\n"
+    sudo mkdir $NEO4J_DIR/import/csps/reports
+    printf "Grant access ...\n"
+    sudo chmod 777 $NEO4J_DIR/import/csps/reports
 
-printf "Normalize data ...\n"
-(docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/data_normalization.cql
-printf "Done.\n"
-
-printf "Export statistics ...\n"
-(docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/export_statistics.cql
-printf "Done.\n"
-
-printf "Snapshot report ...\n"
-(docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/graph_db_report.cql
-printf "Done.\n"
+    printf "Export statistics ...\n"
+    (docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/export_statistics.cql
+    printf "Done.\n"
+  fi
+fi
 
 res2=$(date +%s)
 dt=$(echo "$res2 - $res1" | bc)
