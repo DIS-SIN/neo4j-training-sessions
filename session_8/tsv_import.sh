34168f7e9fed4753fbd28cdbdb98c06c317e5b07
NGHIA DOAN <hong.nghiadoan@da-an.ca>
2019-07-31 05:58:37 -0400

    Fix condition.

--- session_8/tsv_import.sh
+++ session_8/tsv_import.sh
@@ -28,10 +28,9 @@ printf "Done.\n"
 printf "Snapshot report ...\n"
 (docker exec -i $CONTAINER_NAME /var/lib/neo4j/bin/cypher-shell -u $USER_NAME -p $PASSWORD -a bolt://$BOLT_HOST_PORT) < import_scripts/graph_db_report.cql
 printf "Done.\n"
-fi
 
 res2=$(date +%s)
 diff=`echo $((res2-res1)) | awk '{printf "%02dh:%02dm:%02ds\n",int($1/3600),int($1%3600/60),int($1%60)}'`
-printf "\n[Data import] DONE. Total processing time: %s" $diff
+printf "\n[Data import] DONE. Total processing time: %s.\n" $diff
 
 export MSYS_NO_PATHCONV=0
